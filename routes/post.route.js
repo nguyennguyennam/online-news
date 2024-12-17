6cef676acfabe299e87b479f3dd1e5dae788ae93
Admin <135419938+nguyennguyennam@users.noreply.github.com>
2024-12-17 20:18:25 +0700

    fetch wrtier's pages

---
+++ routes/post.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+import {renderWriter, save_writer_post} from "../controllers/writer.controller.js";
+
+const router = express.Router();
+
+
+router.get("/post",  renderWriter);
+router.post("/post", save_writer_post);
+
+export default router;
